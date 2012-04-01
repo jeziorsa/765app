bcb2287bf95d9b0a5974756940c676027efa7887
Adam Jeziorski <biuro@atsa.pl>
2012-04-01 02:27:07 +0200

    Oo

--- engler.py
+++ engler.py
@@ -1,4 +1,20 @@
 import csv
-csvReader = csv.reader(open('wordlist.csv','rb'), delimiter=' ', quotechar='|')
-for row in csvReader:
-    print ', '.join(row)
+
+def import_wordlist(file):
+    csvReader = csv.reader(open(file,'rb'), delimiter=' ', quotechar='|')
+    return csvReader
+
+def clear_list(list):
+    
+    return
+
+def show_list(list):
+    for row in list:
+        print ', '.join(row)
+    return
+
+
+if __name__ == '__main__':
+    list = import_wordlist('wordlist.csv')	
+    print list
+    #show_list(list)
